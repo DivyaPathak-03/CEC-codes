816128db1936ebc1571e08a548dce337ef59f551
DIVYA <71978567+DivyaPathak-03@users.noreply.github.com>
2021-03-04 22:14:33 +0530

    Add files via upload

---
+++ patterns/pattern7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("Enter number of rows ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {   for(int j=0;j<i;j++)
+        {
+            printf(" ");
+        }
+        for(int j=0;j<n-i;j++)
+        {
+            printf("%d",n-i-j);
+        }
+        printf("\n");
+    }
+
+}
